c88bbaf8b7a535c15bcbd6ff0d104a175fefb5dd
Yifan428 <526220305@qq.com>
2017-06-14 15:37:40 -0500

    Commit to GitHub

---
+++ Swift Language Basics/Swift Language Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Language Basics
+//
+//  Created by Shiyifan on 14/06/2017.
+//  Copyright © 2017 Shiyifan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
